7e0e1ee3fa28f8fab9e124416f2900746f2c30cf
anildalar <dollorinfotech@gmail.com>
2021-03-27 07:58:58 +0530

    26MarchCode

---
+++ a.js
@@ -0,0 +1,50 @@
+console.log('Hello World');
+
+//Variables
+
+
+//ES6 = ECMA 2015
+
+var x1=5;
+let y=6;
+const z=7;
+
+console.log(x1+y+z);
+
+
+//Functions.
+
+//Functiona are resuable block of code
+
+
+
+/*
+function functionName(parameters) {
+    // code to be executed
+}
+*/
+
+//1. Function Decleration (One time process)
+//2. Function Calling (Many Time Process)
+
+//Function Declare
+function getMyName(){
+    console.log("Hello Good Morning"); //Statement 1
+    console.log("My Name is Anil"); //Statement 2
+}
+
+//Function Calling
+getMyName();
+getMyName();
+getMyName();
+getMyName();
+
+//Function Decleration
+function funcA(){
+    console.log("Hello from Function A");
+}
+//Function calling
+funcA();
+
+
+
