447bbb02b81c46ae47965a4196ff9bbd05e32cad
anildalar <dollorinfotech@gmail.com>
2021-03-30 08:22:44 +0530

    c.js work string explaintion

---
+++ c.js
@@ -0,0 +1,11 @@
+
+let x=10;
+
+const y=20;
+
+//Single Quote 
+console.log('The Sum is '+ (x+y) );
+//Double Quote
+console.log("The Sum is "+ (x+y) );
+//Backtic 
+console.log(`The Sum is ${x+y} `);
