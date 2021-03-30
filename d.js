a7ea523b7e4a9c4dd607a1cf6c10836bc990a0ad
anildalar <dollorinfotech@gmail.com>
2021-03-30 08:33:37 +0530

    d.js class defination

--- d.js
+++ d.js
@@ -1,1 +1,21 @@
-console.log('Hello from d.js file');
+//Class 
+
+//Class Decleration
+class MyClass{
+   //1. Properties
+   
+   //2. Constructor
+
+   //3. Method // function
+   myFunction(){
+        console.log('Hello Form myFunctiona');
+   }
+
+}
+//Create an object of the class
+
+//let obj = new class();
+let obj = new MyClass();
+//Access the member using object
+//object.member
+obj.myFunction(); // Dot . is member selection operator
