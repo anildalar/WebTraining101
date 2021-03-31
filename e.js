e4ae85eef772238f978f11895072d709f4cff7bb
anildalar <dollorinfotech@gmail.com>
2021-03-31 08:09:55 +0530

    class

--- e.js
+++ e.js
@@ -1,3 +1,28 @@
-console.log('Hello from e.js file');
+//Class Defination
+//PascalCase
+class MyClass{
+    //1.Property
 
-//Hello
+    //2.Constructor
+
+    //3.Method
+    myFunction1(n){ // n is formal argument
+        console.log(`Hello ${n} from myFunction1`);
+        //this is used as internal object
+        this.myFunction2();
+    }
+
+    myFunction2(){
+        console.log("Hello from myFunction2");
+    }
+} //Class Defination End
+
+//Create an external object of the class
+
+//let objectname = new ClassName()
+let x = new MyClass();
+//Access the members using object
+//object.member
+
+x.myFunction1("Anil"); //Actual Argument
+//x.myFunction2();
