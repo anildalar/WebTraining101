18a43992f2e63636b768388bdaf7f517dce41aab
anildalar <dollorinfotech@gmail.com>
2021-03-31 08:47:42 +0530

    new

--- e.js
+++ e.js
@@ -1,19 +1,22 @@
 //Class Defination
 //PascalCase
-class MyClass{
+class MyClass {
     //1.Property
+    x = 10;
+    y = 20;
 
     //2.Constructor
 
     //3.Method
-    myFunction1(n){ // n is formal argument
+    myFunction1(n) { // n is formal argument
         console.log(`Hello ${n} from myFunction1`);
         //this is used as internal object
         this.myFunction2();
     }
 
-    myFunction2(){
+    myFunction2() {
         console.log("Hello from myFunction2");
+        console.log("The sum is "+ (this.x+this.y) );
     }
 } //Class Defination End
 
