5b11663087e14da0608ae9f66ace5f0b0badc87a
anildalar <dollorinfotech@gmail.com>
2021-04-02 08:13:06 +0530

    constructor introduction

---
+++ h.js
@@ -0,0 +1,39 @@
+//Class Defination
+class MyClass{
+    //1. Properties
+    contacts={}; // blank object
+
+    //2. Constructor
+    constructor(y){ //y is formal arg
+        console.log('Hello from constructor');
+        //Constructor is immideatly called when we create the object of the class
+        //The role of constructor is to initialize the properties
+        this.contacts = y;
+
+        //Call the method using internal object
+        this.getContacts();
+    }
+
+    //3.Method
+    getContacts(){
+        console.log(this.contacts);
+    }
+
+
+} //End of Class
+
+//Lets create the ex object of the class
+//let object = new ClassName();
+
+let x = {
+    name:"Anil",
+    surname:"Dollor",
+    mob:"9797797979",
+    addr:"Neemuch MP"
+};
+let object = new MyClass(x);
+
+//Call the method using ex object
+//object.member
+
+object.getContacts();
