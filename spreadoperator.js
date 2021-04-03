7beedc39159982fe9952302abfa6bfa399c59c11
anildalar <dollorinfotech@gmail.com>
2021-04-03 16:35:50 +0530

    spread operator

---
+++ spreadoperator.js
@@ -0,0 +1,4 @@
+var friends = ["Anil","Sunil","Ravi","Vikki"]; //lets create an array of friends
+var friends2 = ['Ram',...friends,"Shyam"]; //Now insert all element of friends array in friends 2 array using spread operator
+
+console.log(friends2); //This will show all friends 
