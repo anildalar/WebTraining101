8726f6239d3fd8101ece88996bdff631db7315e0
anildalar <dollorinfotech@gmail.com>
2021-04-09 08:28:59 +0530

    export1.js

---
+++ export1.js
@@ -0,0 +1,27 @@
+//clas defination
+class MyClass{
+    //1. Property
+    friends=[]; //Empty Array
+
+    //2. constructor
+    constructor(f){ // f = formal arg
+        //The role of constructor is to initialize the members
+        this.friends = f;
+    }
+
+    //3. Method
+    getMyFriends(){
+        return this.friends;
+    }
+
+    getMyFriendsCount(){
+        //Array.length
+        return this.friends.length;
+    }
+
+}//Class Def End
+
+module.exports = MyClass;
+//1. NodeJs   module.exports
+//2. Js  1. named export
+//3. Js  2. default export
